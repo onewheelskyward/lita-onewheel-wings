2b08b269c652eea58b4515f9d0fd2f5c5bddfecc
Andrew Kreps <andrew.kreps@gmail.com>
2015-06-15 13:29:42 -0700

    Wing bomb!

--- lib/lita/handlers/wings.rb
+++ lib/lita/handlers/wings.rb
@@ -2,9 +2,21 @@ module Lita
   module Handlers
     class Wings < Handler
       route /wings/i, :handle_wings
+      route /^!wing bomb$/i, :handle_wing_bomb
+
+      def get_random
+        do_do_rand_rand_rand = Random.new
+        do_do_rand_rand_rand.rand(10000).to_s
+      end
 
       def handle_wings(response)
-        response.reply('http://4gspot.files.wordpress.com/2010/10/1273212602563.gif?' + Time.now.to_i.to_s)
+        response.reply('http://4gspot.files.wordpress.com/2010/10/1273212602563.gif?' + get_random)
+      end
+
+      def handle_wing_bomb(response)
+        1..5.times do
+          response.reply('http://4gspot.files.wordpress.com/2010/10/1273212602563.gif?' + get_random)
+        end
       end
     end
 
--- spec/lita/handlers/wings_spec.rb
+++ spec/lita/handlers/wings_spec.rb
@@ -5,4 +5,10 @@ describe Lita::Handlers::Wings, lita_handler: true do
     send_message ' i like wings i do'
     expect(replies.last).to include('http://4gspot.files.wordpress.com/2010/10/1273212602563.gif')
   end
+
+  it 'wing bombs' do
+    send_message '!wing bomb'
+    expect(replies.count).to eq(5)
+    expect(replies.last).to include('http://4gspot.files.wordpress.com/2010/10/1273212602563.gif')
+  end
 end
